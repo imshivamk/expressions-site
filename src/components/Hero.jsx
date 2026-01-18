198daaa6119a00daa9e5f06743abb3cd3594386a
aayush <hiaayush30@gmail.com>
2026-01-18 13:03:04 +0530

    refactoring

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
-import myImage from "../assets/logo.jpeg"
+import myImage from "../assets/logo.png"
+
+ const currDate  = Date.now();
 
 const Hero = () => {
-  const [date, setDate] = useState(new Date(1770458400000 - Date.now()));
+  const [date, setDate] = useState(new Date(1770458400000 - currDate));
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
+    setInterval(() => {
       setDate(new Date(1770458400000 - Date.now()));
     }, 1000);
   }, []);
@@ -13,60 +15,62 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="relative mt-10
+      className="relative mt-10 
       flex flex-col items-center max-lg:justify-center gap-10
-      pt-6 pb-12 md:pt-32 md:pb-20
+      pt-6 md:pt-32 md:pb-20
       w-full bg-cover bg-center bg-no-repeat
       "
       style={{ backgroundImage: `url(${myImage})` }}
     >
 
 
-      {/* Abstract Shapes */}
-      <div className="absolute top-0 left-0 w-full h-full overflow-hidden opacity-20 pointer-events-none">
-        <div className="absolute -top-24 -left-24 w-96 h-96 bg-blue-500 rounded-full mix-blend-multiply filter blur-3xl animate-blob"></div>
-        <div className="absolute top-0 right-0 w-96 h-96 bg-purple-500 rounded-full mix-blend-multiply filter blur-3xl animate-blob animation-delay-2000"></div>
-      </div>
-
-      <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center text-white">
-        <h1 className="text-5xl font-bold md:text-6xl font-extrabold tracking-tight mb-4">
-          <span className="text-9xl">X</span>PRESSIONS <span className="text-blue-500">2026</span>
-        </h1>
-        <p className="max-w-2xl mx-auto text-lg md:text-xl 
-        text-indigo-100 mb-8 font-semibold">
-          Get Ready for the Most Immersive Techno-Cultural Fest. Innovation
-          meets creativity.
-        </p>
-
-        <div className="flex flex-col sm:flex-row justify-center gap-4 mb-12">
-          <button onClick={()=> window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSddnW3rdwo-u4u7LIf5i_jAu3dQbBRH76M6Q1RDuor_2Mj4AA/viewform?pli=1"} className="bg-white text-indigo-900 font-bold py-3 px-8 rounded-full shadow-lg transform transition hover:scale-105 hover:shadow-xl">
-            Register Now
-          </button>
-          <button onClick={()=> window.location.href = "https://qr.scan.page/uploads/pdf/NdnFm1_6bf5ebd94b5cd184.pdf"}
-           className="border-2 border-white/30 bg-gray-600/40 backdrop-blur-sm text-white font-semibold py-3 px-8 rounded-full hover:bg-white/10 transition">
-            View Rulebook
-          </button>
+      <div className="bg-black/60 p-5 w-full">
+        {/* Abstract Shapes */}
+        <div className="absolute top-0 left-0 w-full h-full overflow-hidden opacity-20 pointer-events-none">
+          <div className="absolute -top-24 -left-24 w-96 h-96 bg-blue-500 rounded-full mix-blend-multiply filter blur-3xl animate-blob"></div>
+          <div className="absolute top-0 right-0 w-96 h-96 bg-purple-500 rounded-full mix-blend-multiply filter blur-3xl animate-blob animation-delay-2000"></div>
         </div>
 
+        <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center text-white">
+          <h1 className="text-5xl font-bold md:text-6xl font-extrabold tracking-tight mb-4">
+            <span className="text-9xl">X</span>PRESSIONS <span className="text-blue-500">2026</span>
+          </h1>
+          <p className="max-w-2xl mx-auto text-lg md:text-xl 
+        text-indigo-100 mb-8 font-semibold">
+            Get Ready for the Most Immersive Techno-Cultural Fest. Innovation
+            meets creativity.
+          </p>
 
-        <div className="grid grid-cols-4 gap-2 max-w-lg mx-auto border border-white/10 bg-gray-900/30 backdrop-blur-sm rounded-xl p-4">
-          <div className="flex flex-col justify-center items-center">
-            <span className="text-2xl">{date.getDate()}</span>
-            <span className="font-semibold text-xl">Days</span>
+          <div className="flex flex-col sm:flex-row justify-center gap-4 mb-12">
+            <button onClick={() => window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSddnW3rdwo-u4u7LIf5i_jAu3dQbBRH76M6Q1RDuor_2Mj4AA/viewform?pli=1"} className="bg-white text-indigo-900 font-bold py-3 px-8 rounded-full shadow-lg transform transition hover:scale-105 hover:shadow-xl">
+              Register Now
+            </button>
+            <button onClick={() => window.location.href = "https://qr.scan.page/uploads/pdf/NdnFm1_6bf5ebd94b5cd184.pdf"}
+              className="border-2 border-white/30 bg-gray-600/40 backdrop-blur-sm text-white font-semibold py-3 px-8 rounded-full hover:bg-white/10 transition">
+              View Rulebook
+            </button>
           </div>
-          <div className="flex flex-col justify-center items-center">
-            <span className="text-2xl">{date.getHours()}</span>
-            <span className="font-semibold text-xl">Hours</span>
+
+
+          <div className="grid grid-cols-4 gap-2 max-w-lg mx-auto border border-white/10 bg-gray-900/30 backdrop-blur-sm rounded-xl p-4">
+            <div className="flex flex-col justify-center items-center">
+              <span className="text-2xl">{date.getDate()}</span>
+              <span className="font-semibold text-xl">Days</span>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <span className="text-2xl">{date.getHours()}</span>
+              <span className="font-semibold text-xl">Hours</span>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <span className="text-2xl">{date.getMinutes()}</span>
+              <span className="font-semibold text-xl">Minutes</span>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <span className="text-2xl">{date.getSeconds()}</span>
+              <span className="font-semibold text-xl">Seconds</span>
+            </div>
+
           </div>
-          <div className="flex flex-col justify-center items-center">
-            <span className="text-2xl">{date.getMinutes()}</span>
-            <span className="font-semibold text-xl">Minutes</span>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <span className="text-2xl">{date.getSeconds()}</span>
-            <span className="font-semibold text-xl">Seconds</span>
-          </div>
-          
         </div>
       </div>
     </section>
