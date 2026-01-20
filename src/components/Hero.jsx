f4e4efed21cdc299ad23d3cb97e7f2a4b5a1c6d3
Shivam Kedar <shivamnkedar1234@gmail.com>
2026-01-20 14:04:38 +0530

    a

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -48,8 +48,8 @@ const Hero = () => {
         <div className="flex flex-col sm:flex-row justify-center gap-4 mb-12">
           <button
             onClick={() =>
-            (window.location.href =
-              "https://docs.google.com/forms/d/e/1FAIpQLSddnW3rdwo-u4u7LIf5i_jAu3dQbBRH76M6Q1RDuor_2Mj4AA/viewform?pli=1")
+              (window.location.href =
+                "https://docs.google.com/forms/d/e/1FAIpQLSddnW3rdwo-u4u7LIf5i_jAu3dQbBRH76M6Q1RDuor_2Mj4AA/viewform?pli=1")
             }
             className="bg-white text-indigo-900 font-bold py-3 px-8 rounded-full shadow-lg transform transition hover:scale-105 hover:shadow-xl"
           >
@@ -57,8 +57,8 @@ const Hero = () => {
           </button>
           <button
             onClick={() =>
-            (window.location.href =
-              "https://qr.scan.page/uploads/pdf/eWNjz9_34eaacca47800a3a.pdf")
+              (window.location.href =
+                "https://qr.scan.page/uploads/pdf/eWNjz9_34eaacca47800a3a.pdf")
             }
             className="border-2 border-white/30 bg-violet-500/40 backdrop-blur-sm text-white font-semibold py-3 px-8 rounded-full hover:bg-white/10 transition"
           >
@@ -67,20 +67,17 @@ const Hero = () => {
 
           <button
             onClick={() =>
-            (window.location.href =
-              "https://www.instagram.com/xpressions_2026")
+              (window.location.href =
+                "https://www.instagram.com/xpressions_2026")
             }
             className="
-              border-2 border-white/30
-              bg-purple-600/40 backdrop-blur-sm
+            bg-white text-indigo-900 font-bold py-3 px-8 rounded-full shadow-lg transform transition hover:scale-105 hover:shadow-xl
               rounded-full
-              p-2
-              hover:bg-white/10
               transition
               flex items-center justify-center
               "
           >
-              <Instagram/>  <p className="px-1">xpressions_2026</p>
+            <Instagram /> <p className="px-1">xpressions_2026</p>
           </button>
 
           <div className=""></div>
